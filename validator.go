a198859fd2aa0ca1af0611e72b8fa5a32380f56a
billcoding <wangmiao07@sina.com>
2021-01-08 17:12:48 +0800

    Add support multiple level struct

--- validator.go
+++ validator.go
@@ -5,14 +5,14 @@ import (
 	"reflect"
 )
 
-//Define Validator struct
+// Validator
 type Validator struct {
 	structPtr interface{}
 	items     []*Item
 	fields    []*reflect.StructField
 }
 
-//New
+// New
 func New(structPtr interface{}) *Validator {
 	items := make([]*Item, 0)
 	fields := reflectx.CreateFromTag(structPtr, &items, "alias", "validate")
@@ -26,26 +26,54 @@ func New(structPtr interface{}) *Validator {
 	}
 }
 
-//Validate
+// Validate
 func (v *Validator) Validate() *Result {
 	ritems := make([]*ResultItem, len(v.items), len(v.items))
 	passedCount := 0
 	for pos, item := range v.items {
 		field := v.fields[pos]
 		value := reflect.ValueOf(v.structPtr).Elem().FieldByName(field.Name)
-		passed, msg := item.Validate(field, value)
-		if passed {
+		resultItem := validate(item, field, value)
+		ritems[pos] = resultItem
+		if resultItem.Passed {
 			passedCount++
-		}
-		ritems[pos] = &ResultItem{
-			Field:   v.fields[pos],
-			Passed:  passed,
-			Message: msg,
 		}
 	}
 	return &Result{
 		StructPtr: v.structPtr,
 		Passed:    len(v.items) == passedCount,
 		Items:     ritems,
+	}
+}
+
+func validate(item *Item, field *reflect.StructField, value reflect.Value) *ResultItem {
+	var innerInterface interface{}
+	switch {
+	case field.Type.Kind() == reflect.Struct:
+		innerInterface = value.Addr().Interface()
+	case field.Type.Kind() == reflect.Ptr && field.Type.Elem().Kind() == reflect.Struct:
+		innerInterface = value.Elem().Addr().Interface()
+	default:
+		passed, msg := item.Validate(field, value)
+		return &ResultItem{Field: field, Passed: passed, Message: msg}
+	}
+	if !item.Required || innerInterface == nil {
+		return &ResultItem{Field: field, Passed: true}
+	}
+	items := make([]*Item, 0)
+	fields := reflectx.CreateFromTag(innerInterface, &items, "alias", "validate")
+	if len(items) != len(fields) {
+		panic("[New]invalid len both items and fields")
+	}
+	v := &Validator{
+		structPtr: innerInterface,
+		items:     items,
+		fields:    fields,
+	}
+	vresult := v.Validate()
+	return &ResultItem{
+		Field:   field,
+		Passed:  vresult.Passed,
+		Message: vresult.Messages(),
 	}
 }
